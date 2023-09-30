2988198d83f3514d46c672da244bd4d3fbe4a4e2
Daniel <72395566+DJcloon@users.noreply.github.com>
2023-09-30 19:44:12 +0900

    Delete CLMS directory
    Files combined into CLMS Application folder

--- CLMS/StandardCabin.java
+++
@@ -1,9 +0,0 @@
-package CLMS;
-
-public class StandardCabin extends Cabin{
-	private static int cabinType = 0;
-		public StandardCabin(String[][] cabinPassenger) {
-		super("Porthole, Toilet,", cabinType, cabinPassenger);
-	}
-
-}
