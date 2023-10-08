200ed96d346d5d4a58a2882e9cf53224d5fad5bd
Daniel <72395566+DJcloon@users.noreply.github.com>
2023-10-08 09:45:06 +0900

    Update CLMS.java

--- application/CLMS.java
+++ application/CLMS.java
@@ -6,49 +6,92 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class CLMS {
-	public static void main(String[] args) {  
-		databaseQuery("*", "ships", "TripLength", 4);
 
-	
-}
-	
-	
-	public static void databaseQuery(String column, String tableName, String searchTerm, String value){  
-		String sql = "SELECT "+column+" FROM "+tableName+ 
-					 " WHERE "+searchTerm+" = "+value;
-		queryCore(CreateShipDatabase.connect(), sql);
-	}  
-	public static void databaseQuery(String column, String tableName, String searchTerm, int value){  
-		String sql = "SELECT "+column+" FROM " +tableName+ " WHERE " +searchTerm+ " = " +value;;  
-		queryCore(CreateShipDatabase.connect(), sql);
+	static CruiseShip[] ships = new CruiseShip[12]; 
+
+	CLMS() {
+		loadShipArray();
+
+	}
+
+	//test for ship array to verify load
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws SQLException {  
+		CLMS clms = new CLMS();
+		for(int i = 0; i < ships.length; i++) {
+			ships[i].printShipDetails();
+		}
+
+		// loop through the result set  
+		ResultSet rs = databaseQuery("ShipName,Maintenance", "ships", "Maintenance", 2021);  
+		while (rs.next()) {  
+			System.out.println(
+					rs.getString("ShipName")+  "\t" +
+					rs.getInt("Maintenance"));
 		}  
-	
-	public static void queryCore(Connection conn, String sql) {
+	}
+
+
+	//method to load array for ship objects from database
+	public static void loadShipArray(){
+		for(int i = 1; i <= ships.length; i++) {
+			queryShipObjectData(DatabaseController.connect(), i);
+		}
+	}
+	//method to query all ships from database and return as ship object  
+	public static void queryShipObjectData(Connection conn, int i) {
+		String sql = "SELECT * FROM ships WHERE id = " + i;
+
 		try {  
 			Statement stmt  = conn.createStatement();  
 			ResultSet rs    = stmt.executeQuery(sql);  
-
-			// loop through the result set  
-			while (rs.next()) {  
-				System.out.println(rs.getInt("id")+  "\t" +
-						rs.getString("ShipName")+  "\t" +
-						rs.getString("ShipCompany")+  "\t" +
-						rs.getString("Location")+  "\t" +
-						rs.getInt("TripLength")+  "\t" + 
-						rs.getInt("Cabins")+  "\t" +
-						rs.getInt("YearOfBuild")+  "\t" +
-						rs.getInt("Maintenance")+  "\t" +
-						rs.getInt("Capacity")+  "\t" +
-						rs.getString("Origin")+  "\t" +
-						rs.getString("FinalDestination")+  "\t" +
-						rs.getString("Destination1")+  "\t" +
-						rs.getString("Destination2")+  "\t" +
-						rs.getString("Destination3")+  "\t" +
-						rs.getString("Destination4")+  "\t" +
-						rs.getString("Destination5"));  
-			}  
+			ships[rs.getInt("id")-1] = new CruiseShip(rs.getInt("id"),
+					rs.getString("ShipName"),
+					rs.getString("ShipCompany"),
+					rs.getString("Location"),
+					rs.getInt("TripLength"), 
+					rs.getInt("Cabins"),
+					rs.getInt("YearOfBuild"),
+					rs.getInt("Maintenance"),
+					rs.getInt("Capacity"),
+					rs.getString("Origin"),
+					rs.getString("FinalDestination"),
+					rs.getString("Destination1"),
+					rs.getString("Destination2"),
+					rs.getString("Destination3"),
+					rs.getString("Destination4"),
+					rs.getString("Destination5")); 
 		} catch (SQLException e) {  
 			System.out.println(e.getMessage());  
-		}  
+		} 
 	}
+
+
+	//method to run SQL database query for string
+	public static ResultSet databaseQuery(String column, String tableName, String searchTerm, String value){  
+		String sql = "SELECT "+column+" FROM "+tableName+ 
+				" WHERE "+searchTerm+" = "+value;
+		ResultSet rs = queryCore(DatabaseController.connect(), sql);
+		return rs;
+	}  
+	//method to run SQL database query for int
+	public static ResultSet databaseQuery(String column, String tableName, String searchTerm, int value){  
+		String sql = "SELECT "+column+" FROM " +tableName+ " WHERE " +searchTerm+ " = " +value;;  
+		ResultSet rs = queryCore(DatabaseController.connect(), sql);
+		return rs;
+	}  
+	//core of SQL database query.  runs query and returns results
+	public static ResultSet queryCore(Connection conn, String sql) {
+		ResultSet rs = null;
+		try {  
+			Statement stmt  = conn.createStatement();  
+			rs    = stmt.executeQuery(sql);  
+		} catch (SQLException e) {  
+			System.out.println(e.getMessage());  
+		}
+		return rs;  
 	}
+
+
+}
+
