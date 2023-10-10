471f22d74ef81ea71be9629da111c2dde1fea4a6
Ti-Andre Sabur <133469879+TIANDRE-S@users.noreply.github.com>
2023-10-10 21:40:31 +0900

    Update DatabaseController.java to import Passenger class from ManifestContent

--- application/DatabaseController.java
+++ application/DatabaseController.java
@@ -8,6 +8,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import application.ManifestContent.Passenger;
+
 
 public class DatabaseController {  
 
